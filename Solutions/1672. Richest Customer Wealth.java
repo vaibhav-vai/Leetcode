0ab11fe9479fe3d447dc96d854046415486fdae0
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-02-03 17:48:32 +0530

    Create 1672. Richest Customer Wealth.java

---
+++ Solutions/1672. Richest Customer Wealth.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        /*
+        // person = rows
+        // account = column
+        int ans = Interger.MIN_VALUE;
+        for(int person = 0; person <accounts.length ; person++ ){
+            // when you start a new row take a new sum for the row
+            int sum=0;
+            for(int account=0; account<accounts[person].length; account++){
+                sum+= accounts[person][account;]
+            }
+            // now we have the sum of the account of the person
+            //check with the overall accounts of the person
+            if(sum>ans){
+                ans = sum;
+            }
+        }
+        return ans;
+        }
+        */
+        int ans = Integer.MIN_VALUE;
+        for(int[] person : accounts){
+            int sum=0;
+            for(int column : person){
+                sum = sum + column;
+            }
+            if(sum>ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
