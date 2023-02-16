b3082f3e7421dfec29f675ed1e88d2c2a2fec899
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-02-16 23:26:58 +0530

    Create 1470. Shuffle the Array.java

---
+++ Solutions/1470. Shuffle the Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        int j =0;
+        for(int i=0; i<2*n ; i = i+2){
+            ans[i] = nums[j];
+            ans[i+1] = nums[j+n];
+            j++;
+        }
+        return ans;
+    }
+}
