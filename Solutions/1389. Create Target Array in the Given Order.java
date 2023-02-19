a766b233aec01d5d15035289cbad20d51b5261e8
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-02-19 22:08:16 +0530

    Create 1389. Create Target Array in the Given Order.java

---
+++ Solutions/1389. Create Target Array in the Given Order.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[index.length];
+        for(int i=0; i<index.length; i++){
+            for(int j = i; j > index[i]; j--){
+                target[j] = target[j - 1];
+            }
+            target[index[i]] = nums[i];
+        }
+        return target;
+    }
+}
