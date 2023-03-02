5a6ff026f1375da032f9530ff67bc3ef64d984a6
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-03-02 11:01:36 +0530

    Create 1886. Determine Whether Matrix Can Be Obtained By Rotation.java

---
+++ Solutions/1886. Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for(int i = 0; i < 4; i++) {
+            if(check(mat, target)) {
+                return true;
+            }
+            rotate(mat);
+        }
+        return false;
+    }
+
+    static boolean check(int[][] mat, int[][] target) {
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat.length; j++){
+                if(mat[i][j] != target[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true; 
+    }
+
+    static void rotate(int[][] mat){
+        for(int i = 0; i < mat.length; i++){
+            for (int j = 0; j < i ; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        int start=0;
+        int end=mat.length-1;
+        while(start <= end){
+            for(int row = 0; row < mat.length; row++){
+                int temp = mat[row][start];
+                mat[row][start] = mat[row][end];
+                mat[row][end] = temp;
+            }
+            start++;
+            end--;
+        }
+    }
+}
