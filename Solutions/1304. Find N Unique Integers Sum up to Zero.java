87e7715bb2b20b8170c6e5aff6680ff3b0e6fc17
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-03-04 10:45:54 +0530

    Create 1304. Find N Unique Integers Sum up to Zero.java

---
+++ Solutions/1304. Find N Unique Integers Sum up to Zero.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/description/
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] arr = new int[n];
+        if(n%2==0){
+            for(int i=0; i<n; i=i+2){
+                arr[i]= i+1;
+                arr[i+1]= -(i+1);
+            }
+        }
+        else{
+            for(int i=0; i<n-1; i=i+2){
+                arr[i]= i+1;
+                arr[i+1]= -(i+1);
+            }
+            arr[n-1] = 0;
+        }
+        return arr;
+    }
+}
