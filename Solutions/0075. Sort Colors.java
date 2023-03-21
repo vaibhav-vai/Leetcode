9713f7cff7eb6820e826cf6a1a9bc28678021604
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-03-21 20:08:30 +0530

    Update 0075. Sort Colors.java

--- Solutions/0075. Sort Colors.java
+++ Solutions/0075. Sort Colors.java
@@ -3,24 +3,30 @@
 
 class Solution {
     public void sortColors(int[] nums) {
-        int l = 0;               // Next 0 should be put in l
-        int r = nums.length - 1; // Next 2 should be put in r
+        int start = 0;
+        int end = nums.length - 1;
+        int index = 0;
 
-    for (int i = 0; i <= r;)
-      if (nums[i] == 0)
-        swap(nums, i++, l++);
-      else if (nums[i] == 1)
-        ++i;
-      else
-        // We may swap a 0 to index i, but we're still not sure whether
-        // this 0 is put in the correct index, so we can't move pointer i
-        swap(nums, i, r--);
-  }
+        if(nums.length == 0 || nums.length == 1){
+            return;
+        }
 
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
-        
+        while(index <= end && start < end){
+            if(nums[index] == 0){
+                nums[index] = nums[start];
+                nums[start] = 0;
+                index++;
+                start++;
+            }
+            else if(nums[index] == 2){
+                nums[index] = nums[end];
+                nums[end] = 2;
+                end--;
+            }
+            else{
+                index++;
+            }
+        }
+
     }
+}
