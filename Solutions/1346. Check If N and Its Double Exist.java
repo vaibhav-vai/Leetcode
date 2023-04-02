5e027c604f0ebef7c9e683e840450b639a7e57d0
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-04-02 21:07:00 +0530

    Create 1346. Check If N and Its Double Exist.java

---
+++ Solutions/1346. Check If N and Its Double Exist.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/submissions/926688833/
+
+
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+       
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if(i!=j && arr[i] == 2*arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
