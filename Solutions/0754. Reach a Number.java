fd4048dfd3c409cf020e2b7423a936827c4071b2
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-04-09 21:28:00 +0530

    Create 0754. Reach a Number.java

---
+++ Solutions/0754. Reach a Number.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/reach-a-number/description/
+
+
+
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int step = 0;
+        int sum = 0;
+        while (sum < target) {
+            step++;
+            sum += step;
+        }
+        while ((sum - target) % 2 != 0) {
+            step++;
+            sum += step;
+        }
+        return step;
+    }
+}
