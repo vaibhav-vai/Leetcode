c1711fadf11663e8d2e8c0f7bfdc29069bacdabe
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-04-14 22:14:40 +0530

    Create 0217. Contains Duplicate.java

---
+++ Solutions/0217. Contains Duplicate.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] == nums[i + 1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
