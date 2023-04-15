a0ba976eb72706c1dc1eb4e85b42d29c6a7f0c09
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-04-15 12:33:29 +0530

    Create 1838. Frequency of the Most Frequent Element.java

---
+++ Solutions/1838. Frequency of the Most Frequent Element.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
+
+
+
+import java.util.*;
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+         Arrays.sort(nums);
+        int n = nums.length;
+        int ans = 1, window = 0;
+        for (int l = 0, r = 1; r < n; ++r) {
+            window += (nums[r] - nums[r - 1]) * (r - l);
+            while (window > k) {
+                window -= (nums[r] - nums[l++]);
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+}
