cf38df32e95669eb71287547ab34e098ca3d3f54
Vaibhav <98206149+vaibhav-vai@users.noreply.github.com>
2023-04-22 18:21:21 +0530

    Create 0268. Missing Number.java

---
+++ Solutions/0268. Missing Number.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/missing-number/description/
+
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+    int ans = nums.length;
+
+    for (int i = 0; i < nums.length; ++i)
+      ans ^= i ^ nums[i];
+
+    return ans;
+    }
+}
